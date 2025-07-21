519cf32dadf6882e44c91693e86337360d2acf4c
Omna Rawat <omnarawat2406@gmail.com>
2025-07-21 21:08:04 +0530

    ATM1.java
    Internship- Task 1

--- ATM1.java
+++ ATM1.java
@@ -1,15 +1,3 @@
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
-/**
- *
- * @author Laxman
- */
-
 import java.util.*;
 public interface ATM1 {
     abstract void customerinfo(String id,String name);
